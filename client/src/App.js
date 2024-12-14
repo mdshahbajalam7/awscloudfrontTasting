da4fbef9d6ff2dfbff93a95626bae6916c794413
Himanshu <98772205+mdshahbajalam7@users.noreply.github.com>
2024-12-14 16:14:05 +0530

    aws done

---
+++ client/src/App.js
@@ -0,0 +1,36 @@
+// import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1
+        style={{
+          color: "red",
+          fontSize: "50px",
+          margin: 0,
+          padding: 0,
+          // border: "1px solid",
+        }}
+      >
+        This is My first Aws CloudFront Testing App
+      </h1>
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+    </div>
+  );
+}
+
+export default App;
